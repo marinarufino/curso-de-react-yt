a0e00d6ae4cff87bab121e6f36810457964db729
marinarufino <marinarufinox@gmail.com>
2025-04-06 19:53:49 -0300

    correção do layout da interface de detalhes da tarefa

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -1,34 +1,37 @@
-import { ChevronLeftIcon } from "lucide-react"
-import { useNavigate, useSearchParams } from "react-router-dom"
-import Title from "../components/Title"
+import { ChevronLeftIcon } from "lucide-react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import Title from "../components/Title";
 
-function TaskPage(){
-    const navigate = useNavigate()
-    const [searchParams]= useSearchParams()
-    const title= searchParams.get("title")
-    const description= searchParams.get("description")
+function TaskPage() {
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const title = searchParams.get("title");
+  const description = searchParams.get("description");
 
-    function onBackClick(){
-        navigate(-1)
-        //poderia ser passada como arrow function pois tem só uma linha, mas também pode ser definida antes e chamada entre chaves no button
-    }
+  function onBackClick() {
+    navigate(-1);
+    //poderia ser passada como arrow function pois tem só uma linha, mas também pode ser definida antes e chamada entre chaves no button
+  }
 
-    return( <div className="h-screen w-screen bg-slate-500 p-6">
-        
-        <div className="w-[500px] space-y-4">
+  return (
+    <div className="h-screen w-screen bg-slate-500 p-6">
+      <div className="w-[500px] mx-auto space-y-4">
         <div className="flex justify-center relative mb-6">
-            <button onClick={onBackClick} className="absolute left-0 top-0 bottom-0 text-slate-100">
-                <ChevronLeftIcon />
-            </button>
-        <Title>Detalhes da tarefa</Title>
+          <button
+            onClick={onBackClick}
+            className="absolute left-0 top-0 bottom-0 text-slate-100"
+          >
+            <ChevronLeftIcon />
+          </button>
+          <Title>Detalhes da tarefa</Title>
         </div>
         <div className="bg-slate-200 p-4 rounded-md">
-            <h2 className="text-x font-bold text-slate-600">{title}</h2>
-            <p className="text-slate-600">{description}</p>
+          <h2 className="text-x font-bold text-slate-600">{title}</h2>
+          <p className="text-slate-600">{description}</p>
         </div>
       </div>
     </div>
-    )
+  );
 }
 
-export default TaskPage
+export default TaskPage;
